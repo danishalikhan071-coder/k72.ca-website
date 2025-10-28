cb8bd8eb6ab49af5719bc6197ff1b3980e1b645b
Mohd Danish Ali <danishalikhan071@gmail.com>
2025-10-28 10:18:07 +0000

    Navbar file name correction

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects.jsx'
-import Navbar from '../src/Components/Navigation/Navbar.jsx'
+import Navbar from './src/Components/Navigation/Navbar.jsx'
 import FullScreenNav from './src/Components/Navigation/FullScreenNav.jsx'
 
 
