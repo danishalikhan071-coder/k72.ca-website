6db3ae09bdf1d862e77a0aac32d687395604dc54
Mohd Danish Ali <danishalikhan071@gmail.com>
2025-10-28 15:23:44 +0530

    Stairs changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Stairs from './components/common/Stairs.jsx'
+import Stairs from './Components/Common/Stairs.jsx'
 import NavContext from './src/Context/NavContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
