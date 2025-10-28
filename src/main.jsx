57a387d348cd1655887956559860f6c5290bc8e9
Mohd Danish Ali <danishalikhan071@gmail.com>
2025-10-28 15:39:51 +0530

    Fix import path for NavContext component

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Stairs from './Components/Common/Stairs.jsx'
-import NavContext from './context/NavContext.jsx'
+import NavContext from './Context/NavContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
